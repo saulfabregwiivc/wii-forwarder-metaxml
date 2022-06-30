be19ae632b158361049c261653d68c6c5c444df6
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2022-06-30 14:47:58 -0500

    Update forwarder code to v14

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,8 @@
-﻿ /****************************************************************************
+ /****************************************************************************
  * Copyright 2009 The Lemon Man and thanks to luccax, Wiipower, Aurelio and crediar
  * Copyright 2010 Dimok
- * Copyright 2011 FIX94, oggzee
- *
- * Original forwarder source by
+ * Copyright 2011 oggzee
+ * Copyright 2011-2012 FIX94
  *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any
@@ -38,9 +37,12 @@
 
 #include "devicemounter.h"
 
-#define EXECUTE_ADDR	((u8 *) 0x92000000)
-#define BOOTER_ADDR		((u8 *) 0x93000000)
-#define ARGS_ADDR		((u8 *) 0x93200000)
+#define EXECUTE_ADDR	((u8 *)0x92000000)
+#define BOOTER_ADDR		((u8 *)0x93000000)
+#define ARGS_ADDR		((u8 *)0x93200000)
+
+#define Priiloader_CFG1	((vu32*)0x8132FFFB)
+#define Priiloader_CFG2	((vu32*)0x817FEFF0)
 
 #define MAX_CMDLINE 4096
 #define MAX_ARGV    1000
@@ -48,56 +50,40 @@ struct __argv args;
 char *a_argv[MAX_ARGV];
 char *meta_buf = NULL;
 
-typedef void (*entrypoint) (void);
+extern "C" {
 extern void __exception_setreload(int t);
 extern void __exception_closeall();
+};
+
+typedef void (*entrypoint) (void);
 extern const u8 app_booter_bin[];
 extern const u32 app_booter_bin_size;
 
-static FILE *open_file(const char *dev, char *filepath)
-{
-	sprintf(filepath, "%s:/apps/usbloader/boot.dol", dev);
-	FILE *exeFile = fopen(filepath ,"rb");
-
-	if (exeFile == NULL)
-	{
-		sprintf(filepath, "%s:/apps/usbloader_cfg/boot.dol", dev);
-		exeFile = fopen(filepath ,"rb");
-	}
-	if (exeFile == NULL)
-	{
-		sprintf(filepath, "%s:/apps/usbloader/boot.elf", dev);
-		exeFile = fopen(filepath ,"rb");
-	}
-	if (exeFile == NULL)
-	{
-		sprintf(filepath, "%s:/apps/usbloader_cfg/boot.elf", dev);
-		exeFile = fopen(filepath ,"rb");
-	}
-
-	return exeFile;
-}
+static const int PathNum = 2;
+static const char *Paths[PathNum] = { "/apps/beebem/boot.dol", "/apps/beebem/boot.elf" };
 
 void SystemMenu()
 {
-	*(vu32*)0x8132FFFB = 0x50756e65;
-	DCFlushRange((void *)(0x8132FFFB), 4);
-	ICInvalidateRange((void *)(0x8132FFFB), 4);
+	/* If priiloader is installed say return to system menu */
+	*Priiloader_CFG1 = 0x50756E65;
+	DCFlushRange((void*)Priiloader_CFG1, 4);
+	*Priiloader_CFG2 = 0x50756E65;
+	DCFlushRange((void*)Priiloader_CFG2, 4);
+	/* Use Return to Menu to exit */
 	SYS_ResetSystem(SYS_RETURNTOMENU, 0, 0);
 }
 
-bool IsDollZ(u8 *buff)
+static bool IsDollZ(u8 *buf)
 {
-	int ret;
+	u8 cmp1[] = {0x3C};
+	return memcmp(&buf[0x100], cmp1, sizeof(cmp1)) == 0;
+}
 
-	u8 dollz_stamp[] = {0x3C};
-	int dollz_offs = 0x100;
-
-	ret = memcmp(&buff[dollz_offs], dollz_stamp, sizeof(dollz_stamp));
-	if (ret == 0)
-		return true;
-
-	return false;
+static bool IsSpecialELF(u8 *buf)
+{
+	u32 cmp1[] = {0x7F454C46};
+	u8 cmp2[] = {0x00};
+	return memcmp(buf, cmp1, sizeof(cmp1)) == 0 && memcmp(&buf[0x24], cmp2, sizeof(cmp2)) == 0;
 }
 
 void arg_init()
@@ -139,40 +125,45 @@ int arg_add(char *arg)
 	return arg_addl(arg, strlen(arg));
 }
 
-int load_file(char *name, void *buf, int size)
-{
-	int fd;
-	int ret;
-	fd = open(name, O_RDONLY);
-	if (fd < 0)
-		return -1;
-	ret = read(fd, buf, size);
-	close(fd);
-	if (ret != size)
-		return -2;
-	return 0;
-}
-
 void load_meta(const char *exe_path)
 {
-	char meta_path[200];
+	char meta_path[ISFS_MAXPATH] ATTRIBUTE_ALIGN(32);
+	memset(meta_path, 0, ISFS_MAXPATH);
 	const char *p;
-	struct stat st;
 
 	p = strrchr(exe_path, '/');
 	snprintf(meta_path, sizeof(meta_path), "%.*smeta.xml", p ? p-exe_path+1 : 0, exe_path);
 
-	if (stat(meta_path, &st) != 0)
-		return;
-	if (st.st_size > 64*1024)
-		return;
-
-	// +1 so that the buf is 0 terminated
-	meta_buf = calloc(st.st_size + 1, 1);
-	if (!meta_buf)
-		return;
-
-	load_file(meta_path, meta_buf, st.st_size);
+	s32 fd = ISFS_Open(meta_path, ISFS_OPEN_READ);
+	if(fd >= 0)
+	{
+		s32 filesize = ISFS_Seek(fd, 0, SEEK_END);
+		ISFS_Seek(fd, 0, SEEK_SET);
+		if(filesize)
+		{
+			meta_buf = (char*)memalign(32, filesize + 1);
+			memset(meta_buf, 0, filesize + 1);
+			ISFS_Read(fd, meta_buf, filesize);
+		}
+		ISFS_Close(fd);
+	}
+	else
+	{
+		FILE *exeFile = fopen(meta_path ,"rb");
+		if(exeFile)
+		{
+			fseek(exeFile, 0, SEEK_END);
+			u32 exeSize = ftell(exeFile);
+			rewind(exeFile);
+			if(exeSize)
+			{
+				meta_buf = (char*)memalign(32, exeSize + 1);
+				memset(meta_buf, 0, exeSize + 1);
+				fread(meta_buf, 1, exeSize, exeFile);
+			}
+			fclose(exeFile);
+		}
+	}
 }
 
 void strip_comments(char *buf)
@@ -236,12 +227,16 @@ void parse_meta()
 	}
 }
 
+static void DeInitDevices()
+{
+	SDCard_deInit();
+	USBDevice_deInit();
+	ISFS_Deinitialize();
+}
+
 int main(int argc, char *argv[])
 {
 	u32 cookie;
-	FILE *exeFile = NULL;
-	void *exeBuffer = (void *)EXECUTE_ADDR;
-	u32 exeSize = 0;
 	entrypoint exeEntryPoint;
 	__exception_setreload(0);
 
@@ -249,77 +244,98 @@ int main(int argc, char *argv[])
 	void *xfb = NULL;
 	GXRModeObj *rmode = NULL;
 
-	VIDEO_Init();	
+	VIDEO_Init();
 	rmode = VIDEO_GetPreferredMode(NULL);
-	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));	
-	VIDEO_Configure(rmode);	
-	VIDEO_SetNextFramebuffer(xfb);	
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	VIDEO_Configure(rmode);
+	VIDEO_SetNextFramebuffer(xfb);
 	VIDEO_SetBlack(TRUE);
 	VIDEO_Flush();
 	VIDEO_WaitVSync();
 	if(rmode->viTVMode&VI_NON_INTERLACE) 
 		VIDEO_WaitVSync();
 
-	char filepath[200];
+	char full_path[ISFS_MAXPATH] ATTRIBUTE_ALIGN(32);
+	memset(full_path, 0, ISFS_MAXPATH);
+	bool FileFound = false;
 
-	//init wiimote
 	WPAD_Init();
 
-	// try SD Card First
-	SDCard_Init();
-	exeFile = open_file(DeviceName[SD], filepath);
-	// if app not found on SD Card try USB
-	if (exeFile == NULL)
+	/* Try NAND (neek2o) first */
+	ISFS_Initialize();
+	for(u8 i = 0; i < PathNum; i++)
 	{
-		USBDevice_Init();
-		int dev;
-		for(dev = USB1; dev < MAXDEVICES; ++dev)
+		strcpy(full_path, Paths[i]);
+		s32 fd = ISFS_Open(full_path, ISFS_OPEN_READ);
+		if(fd >= 0)
 		{
-			if(!exeFile)
-				exeFile = open_file(DeviceName[dev], filepath);
+			s32 filesize = ISFS_Seek(fd, 0, SEEK_END);
+			ISFS_Seek(fd, 0, SEEK_SET);
+			if(filesize)
+			{
+				ISFS_Read(fd, EXECUTE_ADDR, filesize);
+				FileFound = true;
+			}
+			ISFS_Close(fd);
+			break;
 		}
 	}
-
-	// if nothing found exiting
-	if (exeFile == NULL)
+	/* Lets try SD and USB */
+	if(!FileFound)
 	{
-		fclose(exeFile);
-		SDCard_deInit();
-		USBDevice_deInit();
+		for(u8 dev = SD; dev < MAXDEVICES; ++dev)
+		{
+			if(FileFound)
+				break;
+			else if(dev == SD)
+				SDCard_Init();
+			else if(dev == USB1)
+				USBDevice_Init();
+			for(u8 i = 0; i < PathNum; i++)
+			{
+				sprintf(full_path, "%s:%s", DeviceName[dev], Paths[i]);
+				FILE *exeFile = fopen(full_path ,"rb");
+				if(exeFile)
+				{
+					fseek(exeFile, 0, SEEK_END);
+					u32 exeSize = ftell(exeFile);
+					rewind(exeFile);
+					if(exeSize)
+					{
+						fread(EXECUTE_ADDR, 1, exeSize, exeFile);
+						FileFound = true;
+					}
+					fclose(exeFile);
+					break;
+				}
+			}
+		}
+	}
+	if(!FileFound)
+	{
+		DeInitDevices();
 		SystemMenu();
+		return 0;
 	}
-
-	fseek(exeFile, 0, SEEK_END);
-	exeSize = ftell(exeFile);
-	rewind(exeFile);
-
-	if(fread(exeBuffer, 1, exeSize, exeFile) != exeSize)
+	else
 	{
-		fclose(exeFile);
-		SDCard_deInit();
-		USBDevice_deInit();
-		SystemMenu();
+		if(!IsDollZ(EXECUTE_ADDR) && !IsSpecialELF(EXECUTE_ADDR)) 
+		{
+			arg_init();
+			arg_add(full_path); // argv[0] = full_path
+			// load meta.xml
+			load_meta(full_path);
+			// parse <arguments> in meta.xml
+			parse_meta();
+		}
 	}
-	fclose(exeFile);
-
-	if (IsDollZ(exeBuffer) == false) 
-	{
-		arg_init();
-		arg_add(filepath); // argv[0] = filepath
-		// load meta.xml
-		load_meta(filepath);
-		// parse <arguments> in meta.xml
-		parse_meta();
-	}
+	DeInitDevices();
 
 	memcpy(BOOTER_ADDR, app_booter_bin, app_booter_bin_size);
 	DCFlushRange(BOOTER_ADDR, app_booter_bin_size);
 
 	memcpy(ARGS_ADDR, &args, sizeof(args));
 	DCFlushRange(ARGS_ADDR, sizeof(args) + args.length);
-
-	SDCard_deInit();
-	USBDevice_deInit();
 
 	exeEntryPoint = (entrypoint)BOOTER_ADDR;
 	/* cleaning up and load dol */
@@ -328,5 +344,7 @@ int main(int argc, char *argv[])
 	__exception_closeall();
 	exeEntryPoint();
 	_CPU_ISR_Restore(cookie);
+
+	SystemMenu();
 	return 0;
 }
